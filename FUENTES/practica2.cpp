c5b09aad1e0c002efd073fd8dae923f116ced3df
Pedro Bedmar Lopez <correo.pedrobedmar@gmail.com>
2022-05-30 13:45:46 +0200

    p2 -- end p2

--- FUENTES/practica2.cpp
+++ FUENTES/practica2.cpp
@@ -11,6 +11,20 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    int seed = stoi(argv[1]);
+    string algorithm = argv[2];
+    string datafile = argv[3];
+
+    DataLoader data_ = DataLoader(datafile);
+
+    if (algorithm == "g") {
+        GeneticAlgorithm geneticAlgorithm_(data_.getDistanceMatrix(), data_.getNumElements(), data_.getNumRequiredElements(), seed);
+        geneticAlgorithm_.run(1, "gen", "uni");
+    } else if (algorithm == "m") {
+        MemeticAlgorithm memeticAlgorithm_(data_.getDistanceMatrix(), data_.getNumElements(), data_.getNumRequiredElements(), seed);
+        memeticAlgorithm_.run(1, "AM0.1");
+    }
+
 //    DataLoader data_ = DataLoader("data/GKD-b_43_n150_m15.txt");
 //
 //    GeneticAlgorithm geneticAlgorithm_(data_.getDistanceMatrix(), data_.getNumElements(), data_.getNumRequiredElements(), 1);
@@ -68,38 +82,38 @@ int main(int argc, char *argv[]) {
 //    }
 //
 //    cout << endl << endl << endl;
-
-
-    // MEMEMETIC
-    cout << "------ MEMETIC ALGORITHM ------" << endl;
-    vector<string> memeticVersions;
-    memeticVersions.emplace_back("AM1.0");
-    memeticVersions.emplace_back("AM0.1");
-    memeticVersions.emplace_back("AM0.1mej");
-
-    for (auto version: memeticVersions) {
-        list<double> memeticCosts;
-        list<double> memeticTimes;
-        list<double> memeticLowest;
-        list<double> memeticHighest;
-        cout << endl << endl << "-- " << version << " --" << endl;
-
-        for (const auto entry: paths) {
-            DataLoader data = DataLoader(entry);
-
-            cout << endl << "Dataset: " << entry << endl;
-            MemeticAlgorithm memeticAlgorithm(data.getDistanceMatrix(), data.getNumElements(),
-                                              data.getNumRequiredElements());
-            memeticAlgorithm.run(1,version);
-
-            memeticCosts.push_back(memeticAlgorithm.getAvgCost());
-            memeticTimes.push_back(memeticAlgorithm.getAvgTime());
-            memeticLowest.push_back(memeticAlgorithm.getLowestCost());
-            memeticHighest.push_back(memeticAlgorithm.getHighestCost());
-        }
-
-        printResults(memeticCosts, memeticTimes, memeticLowest, memeticHighest);
-    }
-
-    cout << endl << endl;
+//
+//
+//    // MEMEMETIC
+//    cout << "------ MEMETIC ALGORITHM ------" << endl;
+//    vector<string> memeticVersions;
+//    memeticVersions.emplace_back("AM1.0");
+//    memeticVersions.emplace_back("AM0.1");
+//    memeticVersions.emplace_back("AM0.1mej");
+//
+//    for (auto version: memeticVersions) {
+//        list<double> memeticCosts;
+//        list<double> memeticTimes;
+//        list<double> memeticLowest;
+//        list<double> memeticHighest;
+//        cout << endl << endl << "-- " << version << " --" << endl;
+//
+//        for (const auto entry: paths) {
+//            DataLoader data = DataLoader(entry);
+//
+//            cout << endl << "Dataset: " << entry << endl;
+//            MemeticAlgorithm memeticAlgorithm(data.getDistanceMatrix(), data.getNumElements(),
+//                                              data.getNumRequiredElements());
+//            memeticAlgorithm.run(1,version);
+//
+//            memeticCosts.push_back(memeticAlgorithm.getAvgCost());
+//            memeticTimes.push_back(memeticAlgorithm.getAvgTime());
+//            memeticLowest.push_back(memeticAlgorithm.getLowestCost());
+//            memeticHighest.push_back(memeticAlgorithm.getHighestCost());
+//        }
+//
+//        printResults(memeticCosts, memeticTimes, memeticLowest, memeticHighest);
+//    }
+//
+//    cout << endl << endl;
 }
